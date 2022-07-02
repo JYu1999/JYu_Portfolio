1023dbe1f7249a888914a10fd946fd67b8ebdf21
JYu1999 <jk29666338@gmail.com>
2022-07-02 11:38:59 +0800

    Complete resume

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ for(let i=0;i<text.length;i++)
 const lastWord=document.querySelector("#fourteenth");
 const animation = document.querySelector("div.animation");
 lastWord.addEventListener("animationend",()=>{
-    animation.style="transition: all 1s ease; opacity: 0; pointer-events: none;height:0;";
+    animation.style="transition: all 2s ease; opacity: 0; pointer-events: none;height:0;";
 })
