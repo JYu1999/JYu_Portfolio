2514cfb8fde3ee4f43a935878dfe5aeff7cfb851
JYu1999 <jk29666338@gmail.com>
2022-07-03 19:21:11 +0800

    modify animation 2

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const text = document.querySelectorAll(".thePaths");
 
 console.log(text[0].getTotalLength());
 
-for(let i=0;i<text.length;i++)
-{
-    console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
+for (let i = 0; i < text.length; i++) {
+  console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
 }
 
-const lastWord=document.querySelector("#fourteenth");
+const lastWord = document.querySelector("#fourteenth");
 const animation = document.querySelector("div.animation");
-lastWord.addEventListener("animationend",()=>{
-    animation.style="transition: all 2s ease; opacity: 0; pointer-events: none;height:0;";
-})
+lastWord.addEventListener("animationend", () => {
+  animation.style =
+    "transition: all 2s ease; opacity: 0; pointer-events: none;height:200vh";
+});
